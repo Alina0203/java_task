e1af5ee7039c6710d8d8cdd0b4ec5ee0d7cf65ef
Alina0203 <36502066+Alina0203@users.noreply.github.com>
2018-10-10 00:15:37 +0300

    Add files via upload

---
+++ Calculator/src/by/mbicycle/calculator/Runner.java
@@ -0,0 +1,20 @@
+package by.mbicycle.calculator;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Runner {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = null;
+		scanner = new Scanner(System.in);
+		while (scanner.hasNext()) {
+			String expression = scanner.next();
+			System.out.println(expression);
+			// System.out.print(Operation.calculate(expression)+ "\n");
+			BigDecimal result = Operation.calculate(expression);
+			System.out.println(result);
+		}
+	}
+}
